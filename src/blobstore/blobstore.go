2e634a76ac22f2d3cee2f7477566554e79287609
Ross Jones <rossdjones@gmail.com>
2012-02-24 19:31:00 +0000

    I am going to keep forgetting to run go fmt, I just know it

--- src/blobstore/blobstore.go
+++ src/blobstore/blobstore.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
 	"github.com/kless/goconfig/config"
 	"io"
 	"log"
@@ -18,8 +18,9 @@ func DataServer(w http.ResponseWriter, req *http.Request) {
 }
 
 var configFile = flag.String("c", "config.ini", "Location of the config file")
+
 func main() {
-    flag.Parse()
+	flag.Parse()
 
 	c, err := config.ReadDefault(*configFile)
 	if err != nil {
@@ -28,12 +29,12 @@ func main() {
 
 	host, _ := c.String("network", "listen-ip")
 	port, _ := c.Int("network", "listen-port")
-    listen  := fmt.Sprintf("%s:%d", host, port)
+	listen := fmt.Sprintf("%s:%d", host, port)
 
 	http.HandleFunc("/metadata", MetadataServer)
 	http.HandleFunc("/data", DataServer)
 
-    log.Println("Server listening: ", listen)
+	log.Println("Server listening: ", listen)
 	err = http.ListenAndServe(listen, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
