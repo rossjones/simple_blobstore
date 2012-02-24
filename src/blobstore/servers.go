d23e82037858b284b04869c788a2ec7e0a34fb63
Ross Jones <rossdjones@gmail.com>
2012-02-24 19:43:35 +0000

    Adding a license and splitting out some code

--- src/blobstore/servers.go
+++ src/blobstore/servers.go
@@ -18,33 +18,15 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "github.com/kless/goconfig/config"
-    "log"
+    "io"
     "net/http"
 )
 
-var configFile = flag.String("c", "config.ini", "Location of the config file")
-
-func main() {
-    flag.Parse()
-
-    c, err := config.ReadDefault(*configFile)
-    if err != nil {
-        log.Fatal("Couldn't find the config file: ", err)
-    }
-
-    host, _ := c.String("network", "listen-ip")
-    port, _ := c.Int("network", "listen-port")
-    listen := fmt.Sprintf("%s:%d", host, port)
-
-    http.HandleFunc("/metadata", MetadataServer)
-    http.HandleFunc("/data", DataServer)
-
-    log.Println("Server listening: ", listen)
-    err = http.ListenAndServe(listen, nil)
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+func MetadataServer(w http.ResponseWriter, req *http.Request) {
+    io.WriteString(w, "Metadata\n")
 }
+
+func DataServer(w http.ResponseWriter, req *http.Request) {
+    io.WriteString(w, "Data\n")
+}
+
