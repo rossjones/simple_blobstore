0f27d9a6838b811b46c4243c769a1506b91bf951
Ross Jones <rossdjones@gmail.com>
2012-02-24 19:35:38 +0000

    Added a quick script for formatting and changed the tabwidth, not that keen on a default of 8

---
+++ format.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gofmt -l -w -tabwidth=4 -tabs=false src/blobstore/
--- src/blobstore/blobstore.go
+++ src/blobstore/blobstore.go
@@ -1,42 +1,42 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"github.com/kless/goconfig/config"
-	"io"
-	"log"
-	"net/http"
+    "flag"
+    "fmt"
+    "github.com/kless/goconfig/config"
+    "io"
+    "log"
+    "net/http"
 )
 
 func MetadataServer(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Metadata\n")
+    io.WriteString(w, "Metadata\n")
 }
 
 func DataServer(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Data\n")
+    io.WriteString(w, "Data\n")
 }
 
 var configFile = flag.String("c", "config.ini", "Location of the config file")
 
 func main() {
-	flag.Parse()
+    flag.Parse()
 
-	c, err := config.ReadDefault(*configFile)
-	if err != nil {
-		log.Fatal("Couldn't find the config file: ", err)
-	}
+    c, err := config.ReadDefault(*configFile)
+    if err != nil {
+        log.Fatal("Couldn't find the config file: ", err)
+    }
 
-	host, _ := c.String("network", "listen-ip")
-	port, _ := c.Int("network", "listen-port")
-	listen := fmt.Sprintf("%s:%d", host, port)
+    host, _ := c.String("network", "listen-ip")
+    port, _ := c.Int("network", "listen-port")
+    listen := fmt.Sprintf("%s:%d", host, port)
 
-	http.HandleFunc("/metadata", MetadataServer)
-	http.HandleFunc("/data", DataServer)
+    http.HandleFunc("/metadata", MetadataServer)
+    http.HandleFunc("/data", DataServer)
 
-	log.Println("Server listening: ", listen)
-	err = http.ListenAndServe(listen, nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+    log.Println("Server listening: ", listen)
+    err = http.ListenAndServe(listen, nil)
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
 }
